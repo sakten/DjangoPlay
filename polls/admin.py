84d1f8107cefd7ddb9bcda4750cdaa9adee07e1e
sakten <viktor@duhin.ru>
2018-05-29 19:57:19 +0500

    1BP

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Quest, Player, Reward
+
 # Register your models here.
+admin.site.register(Quest)
+admin.site.register(Player)
+admin.site.register(Reward)
+
--- polls/models.py
+++ polls/models.py
@@ -1,34 +1,38 @@
 from django.db import models
 from enum import Enum
 
-DEFAULT_REWARD_ID = 1;
+DEFAULT_REWARD_ID = 1
+
 class PlayerClassChoise(Enum):
-    RG = "Rogue"
-    WR = "Warior"
-    MG = "Mage"
+    def __str__(self):
+        return str(self.value)
+    Rogue = "Rogue"
+    Warior = "Warior"
+    Mage = "Mage"
 
 # Create your models here.
 class Player(models.Model):
 
     def __str__(self):
-        return self.playerClass + " lvl " + self.playerLvl
+        return  self.playerClass + " lvl " + str(self.playerLvl)
+
     playerClass = models.CharField(
-        max_length=5,
-        choices=[(tag,tag.value) for tag in PlayerClassChoise]
+        max_length=10,
+        choices=[(tag, tag.value) for tag in PlayerClassChoise]
     )
     playerLvl = models.IntegerField(default=0)
 
 class Reward(models.Model):
 
     def __str__(self):
-        return "Reward is " + self.size + " gold and " + self.artefacts
+        return "Reward is " + str(self.size) + " gold and " + self.artefacts
     size = models.IntegerField()
     artefacts =  models.CharField(max_length=255,default = "Nothing")     
 
 class Quest(models.Model):
 
     def __str__(self):
-        return "Quest for "+self.preferedClass + " lvl " + self.requiredLvl
+        return "Quest for "+self.preferedClass + " lvl " + str(self.requiredLvl)
     reward = models.ForeignKey(Reward, on_delete=models.CASCADE, default=DEFAULT_REWARD_ID)
     preferedClass =models.CharField (
         max_length=5,
